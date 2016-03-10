9d073fcd5bfe31a703fc791009b5aed5f8988ce1
Zach Ballard <zbginji@gmail.com>
2016-03-10 14:41:12 -0600

    initial structure complete

---
+++ EmployeeReviewsFront/Department.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeReviewsFront
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public List<Employee> EmployeeList { get; set; }
+
+        public decimal TotalSalary()
+        {
+            return EmployeeList.Sum(e => e.Salary);
+        }
+
+        public void EvalutateEmployees()
+        {
+            foreach (var e in EmployeeList)
+            {
+                e.isSatisfactory = EvaluationCheck(e);
+            }
+        }
+
+        public void GiveRaise(decimal r)
+        {
+            decimal empraise = r/EmployeeList.Count;
+            foreach (var e in EmployeeList)
+            {
+                e.Salary += r;
+            }
+        }
+
+        public void AddEmployee(Employee e)
+        {
+            EmployeeList.Add(e);
+        }
+
+        public bool EvaluationCheck(Employee e)
+        {
+            return true;
+        }
+    }
+}
---
+++ EmployeeReviewsFront/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeReviewsFront
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public string Email { get; set; }
+        public string PhoneNum { get; set; }
+
+        public string Review { get; set; }
+        public bool isSatisfactory { get; set; }
+
+        public Employee(string n, decimal s, string e, string p)
+        {
+            Name = n;
+            Salary = s;
+            Email = e;
+            PhoneNum = p;
+        }
+    }
+}
